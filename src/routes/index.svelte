13b2ad78a5102c1d8a42bef9bf0bc95a8009e127
Alex <knyazev.al.st@gmail.com>
2019-08-03 00:06:35 +0300

    Started site development

---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+<svelte:head>
+  <title>Alex's site.</title>
+</svelte:head>
+
+<main>
+  <h1>Alex's site.</h1>
+
+  <p>Hi. I'm Alex.</p>
+
+  <p>I'm student and developer.</p>
+
+  <h2>Blog</h2>
+
+  <ul>
+    <li>Nothing here yet.</li>
+  </ul>
+
+  <h2>Projects</h2>
+
+  <ul>
+    <li> <a href="/">Personal site</a> </li>
+    <li> <a href="TODO">CSS theme</a> </li>
+  </ul>
+
+</main>
