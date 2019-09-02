700e8525c6d68be6a66d8298816e646232a23956
Alexei Knyazev <52626785+alex-knyaz@users.noreply.github.com>
2019-09-03 00:08:11 +0300

    Blog post (#6)
    * Update
    
    * Added more web gems.
    
    * More web gems.
    
    * update

---
+++ src/routes/blog/[slug].svelte
@@ -0,0 +1,11 @@
+<script>
+    import Page from './WebGems.svx';
+    import Layout from '../_layout_exp.svelte'
+</script>
+
+<Layout>
+<span slot="title">Alex's site.</span>
+  <span slot="subtitle">It's interesting, I promise!</span>
+
+<div slot="default"><Page /></div>
+</Layout>
